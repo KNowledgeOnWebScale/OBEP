fc4b71ab6d1dd5752d3d02c67085581d2fbf6a80
pbonte <pieter.bonte@intec.ugent.be>
2016-11-03 10:23:23 +0100

    Added event definitions in OBEP Query

--- src/main/java/sr/obep/OBEPQuery.java
+++ src/main/java/sr/obep/OBEPQuery.java
@@ -1,7 +1,17 @@
 package sr.obep;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+
+import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
+
 /**
  * Created by Riccardo on 03/11/2016.
  */
 public class OBEPQuery {
+	
+	public Set<OWLSubClassOfAxiom> getEventDefinitions(){
+		return Collections.EMPTY_SET;
+	}
 }
